2a895f31bfb7e6156eb835af0af2969b666a6efb
wonderboy002 <nipunkup@gmail.com>
2024-04-22 11:12:01 +0530

    Some errors found during db connection will resolve it kater

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+const app=express();
+require("dotenv").config();
+async function connectDB(){
+    try {
+      await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+      app.listen(process.env.PORT,()=>{
+        console.log(`Server listening on port ${process.env.PORT}`);
+      });
+    }
+    catch(e){
+        console.log("Error while connecting to database ",e);
+    }
+}
+connectDB();
